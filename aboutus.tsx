e32b0abae900a2cea7b0550c1d55823d5a57dd0e
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-29 18:52:02 +0100

    feat: about us modules

---
+++ aboutus.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/components/Restyle";
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">About Us</Text>
+    </Box>
+  );
+};
+
+export default AboutUs;
